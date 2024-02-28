2895ee3cc2561ff49781f11d4ca2728095a42b60
Nursaya <n_saparbay@kbtu.kz>
2024-02-29 00:17:31 +0600

    commit

--- lab4/my-app/src/app/product-alerts/product-alert.component.ts
+++ lab4/my-app/src/app/product-alerts/product-alert.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
+
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css'],
+  styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent {
   @Input() product: Product | undefined;
---
+++ lab4/my-app/src/app/productCategories.ts
@@ -0,0 +1,26 @@
+export interface ProductCategory {
+    name: string;
+    img: string;
+
+  }
+  
+  export const productCategories = [
+    {
+     name: 'phone',
+     img: 'https://kaspi.kz/img/Phone.png'
+
+    },
+    {
+      name: 'laptop',
+      img: 'https://kaspi.kz/img/Computer.png'
+    },
+    {
+      name: 'tv',
+      img: 'https://kaspi.kz/img/TV.png'
+    },
+    {
+      name: 'beauty',
+      img: 'https://resources.cdn-kaspi.kz/shop/static/main-nav/362c1530-38f2-4bfa-b65d-0d4e84f73f5e-Beauty.png'
+    },
+    
+  ];
